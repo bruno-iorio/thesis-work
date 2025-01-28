f757092cbaeecd34728c978e708f4d6c358c57b2
bruno-iorio <bruno.fernandes.iorio@gmail.com>
2025-01-28 12:32:36 +0100

    remove folder

--- models/NN_model/setup.sh
+++
@@ -1,9 +0,0 @@
-pip install nltk
-pip install torch
-pip install pandas
-pip install gensim
-pip install datasets
-pip install matplotlib
-pip install tqdm
-pip install torchinfo
-pip install requests json
